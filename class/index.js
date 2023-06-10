6c30ad7161e51996b113f1efae83efa7fd4d5cc2
Nino Shubitidze <132759949+Nin15@users.noreply.github.com>
2023-06-10 15:19:50 +0400

    Add files via upload

---
+++ class/index.js
@@ -0,0 +1,18 @@
+function buttonClick1(){
+   document.querySelector('.container').style.display = 'block';
+}
+
+function buttonClick2(){
+   document.querySelector('.container').style.display = 'none';
+   
+ }
+ 
+ function buttonClick3(){
+   document.querySelector('.container').style.backgroundColor = 'pink';
+
+
+
+ }
+ 
+ 
+ 
